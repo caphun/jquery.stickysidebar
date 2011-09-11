86d14e78f73ae647dfc255f7c4f001a8ec68cb93
caphun <caphun@pazu2k.local>
2011-09-11 10:25:35 +0800

    added more inline comments

--- jquery.stickysidebar.js
+++ jquery.stickysidebar.js
@@ -54,8 +54,14 @@ $.stickySidebar.prototype = {
     // define self
     var self = this;
 
-    // cache previous scrollTop value
-    self.element.data(stickyPrevScrollTop, -1).css('display', 'inline');
+    self.element
+
+      // cache previous scrollTop value
+      .data(stickyPrevScrollTop, -1)
+
+      // force display inline to leave the element in it's 
+      // position when switched to fixed|absolute
+      .css('display', 'inline');
 
     // here comes the magic!
     $( window ).bind('scroll'+namespace, function() {
@@ -66,14 +72,19 @@ $.stickySidebar.prototype = {
   
   // actually, all the logic is here!!!
   stickiness: function() {
+
     var scrollTop = $( window ).scrollTop(),
         elem = this.element, elemTop = elem.offset().top, elemHeight = elem.outerHeight(),
         pTop = this.parentElem.offset().top, pHeight = this.parentElem.outerHeight(),
         down = (scrollTop > elem.data(stickyPrevScrollTop));
 
-    return elem.css( 
+    return elem.css(
+        // if reached the element minus some gutter space we're entering sticky territory.
         scrollTop > pTop - this.options.gutter
-        && ( (down && (pTop + pHeight > elemTop + elemHeight)) || (!down && scrollTop <= elemTop) )
+        // if scrolling down check if reached the bottom of the parent container
+        && ( (down && (pTop + pHeight > elemTop + elemHeight)) 
+          // if scrolling up check if scrollTop is less than the element top position
+          || (!down && scrollTop <= elemTop) )
 
         // sticky
         ? { 
